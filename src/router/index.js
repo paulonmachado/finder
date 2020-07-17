2598a0c678a2cf2e6c43748d2cbc79b457bcd400
paulonmachado <paulonmachado@me.com>
2020-07-17 03:03:17 +0100

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,5 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Poodle from "../views/Poodle.vue";
-import Finder from "../views/Finder.vue";
 import Quem from "../views/Quem.vue";
 
 Vue.use(VueRouter);
@@ -9,19 +7,9 @@ Vue.use(VueRouter);
 const routes = [
   {
     path: "/",
-    name: "poodle",
-    component: Poodle,
-  },
-  {
-    path: "/finder",
-    name: "finder",
-    component: Finder,
-  },
-  {
-    path: "/quem",
     name: "quem",
     component: Quem,
-  },
+  }
 ];
 
 const router = new VueRouter({
